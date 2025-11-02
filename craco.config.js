e4e91a8afb5e905368903d1a731e2c7977f35cae
Reemark <lordelesly@gmail.com>
2025-11-02 13:20:18 +0100

    Fix: Attempt to fix React useState error by simplifying craco.config.js and removing duplicate craco dependency

--- craco.config.js
+++ craco.config.js
@@ -3,39 +3,6 @@ const webpack = require('webpack');
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // ----------------- Gestion TypeScript avec Babel -----------------
-      const tsRule = webpackConfig.module.rules.find(rule =>
-        rule.test && (rule.test instanceof RegExp ? rule.test.test('.ts') :
-        Array.isArray(rule.test) && rule.test.some(t => t instanceof RegExp && t.test('.ts')))
-      );
-
-      if (tsRule) {
-        const babelLoader = tsRule.use && tsRule.use.find(loader => loader.loader && loader.loader.includes('babel-loader'));
-        if (babelLoader) {
-          babelLoader.options = babelLoader.options || {};
-          babelLoader.options.plugins = babelLoader.options.plugins || [];
-          babelLoader.options.plugins.push(require.resolve('@babel/plugin-transform-typescript'));
-        } else {
-          webpackConfig.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            loader: require.resolve('babel-loader'),
-            options: {
-              presets: [require.resolve('babel-preset-react-app')],
-              plugins: [require.resolve('@babel/plugin-transform-typescript')],
-            },
-          });
-        }
-      } else {
-        webpackConfig.module.rules.push({
-          test: /\.(ts|tsx)$/,
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [require.resolve('babel-preset-react-app')],
-            plugins: [require.resolve('@babel/plugin-transform-typescript')],
-          },
-        });
-      }
-
       // ----------------- Plugin DefinePlugin pour __DEV__ -----------------
       webpackConfig.plugins = webpackConfig.plugins || [];
       webpackConfig.plugins.push(
